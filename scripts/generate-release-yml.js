7c82f7d01961310c774a5376e1480da930156e79
fangyankai <575981390@qq.com>
2025-03-31 22:34:56 +0800

    feat: build

--- scripts/generate-release-yml.js
+++ scripts/generate-release-yml.js
@@ -4,8 +4,6 @@ const path = require('path');
 const { exec } = require('child_process');
 
 const version = require('../package.json').version;
-const zipName = `page-query-text-v${version}.zip`;
-const zipPath = path.join(__dirname, '../build-assets', zipName);
 
 // 读取release-template.yml
 const template = fs.readFileSync(
